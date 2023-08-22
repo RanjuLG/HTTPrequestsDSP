669f7e519bb064e8c49733c7502e04ab4a2f098b
RanjuLG <ranjue16@gmail.com>
2023-08-22 10:37:28 +0530

    Please review this

---
+++ GETrequests/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HttpClientStatus;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var client = new HttpClient();
+
+        var result = await client.GetAsync("http://10.20.30.198:8013/api/status");
+        Console.WriteLine(result.StatusCode);
+    }
+}
