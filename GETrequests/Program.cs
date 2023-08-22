423e0f3c7ed3093955bdfd83e95805c2a6cc7d8f
RanjuLG <ranjue16@gmail.com>
2023-08-22 12:20:17 +0530

    Status code and the content

--- GETrequests/Program.cs
+++ GETrequests/Program.cs
@@ -1,12 +1,32 @@
-﻿namespace HttpClientStatus;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+namespace HTTPClient;
 
 class Program
 {
     static async Task Main(string[] args)
-    {
-        using var client = new HttpClient();
 
-        var result = await client.GetAsync("http://10.20.30.198:8013/api/status");
-        Console.WriteLine(result.StatusCode);
+    {
+        try
+        {
+
+
+            var url = "http://10.20.30.198:8013/api/status/";
+
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync(url);
+            var content = await client.GetStringAsync(url);
+            Console.WriteLine(result.StatusCode);
+           Console.WriteLine(content);
+        }
+        catch (Exception ex) { 
+        
+            Console.WriteLine("The error: "+ ex.Message);        
+        }
+
+
     }
 }
